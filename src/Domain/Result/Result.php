8ed6a071b88d08068655e081c0dc92b4b00ed44c
Berry Ligtermoet <dev@krypt.nl>
2021-08-26 00:34:18 +0200

    Created Result domain models

---
+++ src/Domain/Result/Result.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Paragin\Cli\Domain\Result;
+
+/**
+ * Single tuple of a result collection and a candidate name
+ *
+ * @package Paragin\Cli\Domain\Result
+ */
+class Result
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var float[]
+     */
+    private $scores;
+
+    /**
+     * Result constructor.
+     *
+     * @param string $name
+     * @param float[] $scores
+     */
+    public function __construct(string $name, array $scores)
+    {
+        $this->name = $name;
+        $this->scores = $scores;
+    }
+
+    /**
+     * @return array
+     */
+    public function getScores(): array
+    {
+        return $this->scores;
+    }
+
+    /**
+     * Retrieve a score for a particular question
+     *
+     * @param int $i
+     *
+     * @return float
+     */
+    public function getScoreAt(int $i): float
+    {
+        return $this->scores[$i];
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
---
+++ src/Domain/Result/ResultCollection.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Paragin\Cli\Domain\Result;
+
+/**
+ * Collection class for all information contained in Result csv.
+ *
+ * @package Paragin\Cli\Domain\Result
+ */
+class ResultCollection
+{
+    /**
+     * @var Result[]
+     */
+    private $results;
+
+    /**
+     * @var float[]
+     */
+    private $maximumScores;
+
+    /**
+     * ResultCollection constructor.
+     *
+     * @param Result[] $results
+     * @param float[] $maximumScores
+     */
+    public function __construct(array $results, array $maximumScores)
+    {
+        $this->results = $results;
+        $this->maximumScores = $maximumScores;
+    }
+
+    /**
+     * @return Result[]
+     */
+    public function getResults(): array
+    {
+        return $this->results;
+    }
+
+    /**
+     * @return float[]
+     */
+    public function getMaximumScores(): array
+    {
+        return $this->maximumScores;
+    }
+}

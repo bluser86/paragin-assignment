7559f782bd67825d4581c2e316195523ad61514c
Berry Ligtermoet <dev@krypt.nl>
2021-08-26 03:43:24 +0200

    Add unit test for statistics helper

---
+++ tests/Helper/StatisticsHelperTest.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Paragin\Cli\Helper;
+
+use Paragin\Cli\Exception\Statistics\StatisticsCalculationException;
+use PHPUnit\Framework\TestCase;
+
+class StatisticsHelperTest extends TestCase
+{
+    /**
+     * Test average calculation
+     */
+    public function testAverageCalculation(): void
+    {
+        $helper = new StatisticsHelper();
+
+        $avg = $helper->average([2, 4, 6, 8]);
+
+        $this->assertSame(5.0, $avg);
+    }
+
+    /**
+     * Test average calculation throws on empty collection.
+     */
+    public function testAverageCalculationThrowsOnEmptyCollection(): void
+    {
+        $this->expectException(StatisticsCalculationException::class);
+
+        $helper = new StatisticsHelper();
+
+        $helper->average([]);
+
+    }
+
+    /**
+     * Test p-value calculation.
+     */
+    public function testPValueCalculation(): void
+    {
+        $helper = new StatisticsHelper();
+
+        $p = $helper->pValue([1, 1, 1, 1], 2);
+
+        $this->assertSame(0.5, $p);
+    }
+
+    /**
+     * Test p-value calculation throws when provided an empty score collection.
+     */
+    public function testPValueCalculationThrowsOnEmptyScoreCollection(): void
+    {
+        $this->expectException(StatisticsCalculationException::class);
+
+        $helper = new StatisticsHelper();
+
+        $helper->pValue([], 2);
+    }
+
+    /**
+     * Test Pearson correlation calculation.
+     */
+    public function testPearsonCorrelationCalculation(): void
+    {
+        $helper = new StatisticsHelper();
+
+        $r = $helper->pearsonCorrelation([1, 2, 1, 1, 2], [2, 1, 2, 2, 2]);
+
+        $this->assertSame(-0.6123724356957944, $r);
+    }
+
+    /**
+     * Test Pearson correlation calculation throws on mismatched collections.
+     */
+    public function testPearsonCorrelationCalculationThrowsOnMismatchedCollections(): void
+    {
+        $this->expectException(StatisticsCalculationException::class);
+
+        $helper = new StatisticsHelper();
+
+        $helper->pearsonCorrelation([1, 1, 1], [1, 1]);
+    }
+
+    /**
+     * Test Pearson correlation calculation throws on empty collections
+     */
+    public function testPearsonCorrelationCalculationThrowsOnEmptyScoreCollections(): void
+    {
+        $this->expectException(StatisticsCalculationException::class);
+
+        $helper = new StatisticsHelper();
+
+        $helper->pearsonCorrelation([], []);
+    }
+
+    /**
+     * Test Pearson correlation calculation returns zero on no deviation
+     */
+    public function testPearsonCorrelationCalculationReturnsZeroOnNoDeviation(): void
+    {
+        $helper = new StatisticsHelper();
+
+        $r = $helper->pearsonCorrelation([1, 1, 1], [1, 2, 3]);
+
+        $this->assertSame(0.0, $r);
+
+        $r = $helper->pearsonCorrelation([1, 2, 3], [1, 1, 1]);
+
+        $this->assertSame(0.0, $r);
+    }
+
+    /**
+     * @param $score
+     * @param $max
+     * @param $caesura
+     * @param $floor
+     * @param $expected
+     *
+     * @dataProvider nonLinearCaesuraGradeProvider
+     */
+    public function testNonLinearCaesuraGradeCalculation($score, $max, $caesura, $floor, $expected): void
+    {
+        $helper = new StatisticsHelper();
+
+        $grade = $helper->nonLinearCaesuraGrade($score, $max, $caesura, $floor);
+
+        $this->assertSame($expected, $grade);
+    }
+
+    /**
+     * @return array
+     */
+    public function nonLinearCaesuraGradeProvider(): array
+    {
+        return [
+            [100, 100, 0.7, 0.2, 10.0],
+            [20, 100, 0.7, 0.2, 1.0],
+            [70, 100, 0.7, 0.2, 5.5],
+            [40, 100, 0.7, 0.2, 2.8],
+        ];
+    }
+}

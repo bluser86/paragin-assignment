33fdb543f465802cd5bb83acf98b6e06b987202f
Berry Ligtermoet <dev@krypt.nl>
2021-08-26 02:09:33 +0200

    Added grade command. Displays an overview of student grades based on a test result csv

---
+++ src/Command/GradeCommand.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Paragin\Cli\Command;
+
+use Paragin\Cli\Domain\Result\Grader\ResultGrader;
+use Paragin\Cli\Domain\Result\Parser\ResultParser;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Command outputting all grades for a test result.
+ *
+ * @package Paragin\Cli\Command
+ */
+class GradeCommand extends Command
+{
+    /**
+     * @var ResultParser
+     */
+    private $resultParser;
+
+    /**
+     * @var ResultGrader
+     */
+    private $resultGrader;
+
+    /**
+     * @var string
+     */
+    private $varDir;
+
+    /**
+     * GradeCommand constructor.
+     *
+     * @param ResultParser $resultParser
+     * @param string $varDir
+     */
+    public function __construct(ResultParser $resultParser, ResultGrader $resultGrader, string $varDir)
+    {
+        parent::__construct('grade');
+
+        $this->resultParser = $resultParser;
+        $this->varDir = $varDir;
+        $this->resultGrader = $resultGrader;
+    }
+
+    /**
+     * Configure
+     */
+    protected function configure()
+    {
+        $this->setDescription('Creates a grade overview based on a correctly formatted test result CSV file');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $resultCollection = $this->resultParser->getResults($this->varDir . '/Assignment.csv');
+
+        $table = new Table($output);
+        $table->setHeaders([
+            'Name',
+            'Grade',
+        ]);
+
+        foreach($resultCollection->getResults() as $result) {
+            $grade = $this->resultGrader->calculateGrade($result->getTotalScore(), $resultCollection->getTotalMaximumScore());
+
+            $table->addRow([$result->getName(), number_format($grade, 1)]);
+        }
+
+        $table->render();
+
+        return 0;
+    }
+
+}
